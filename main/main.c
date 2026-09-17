829903aa75b181ca8a8f7e645756448df8aea23a
Benjamin Bodnar <benjamin0906@gmail.com>
2026-09-17 22:07:37 +0200

    Removed unnecessary scan response

--- main/main.c
+++ main/main.c
@@ -53,11 +53,6 @@ static uint8_t adv_raw_data[] = {
 
 static uint8_t temp_raw_data[31] = {};
 
-static uint8_t scan_rsp_raw_data[] = {
-    0x08, ESP_BLE_AD_TYPE_LE_DEV_ADDR, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x11, ESP_BLE_AD_TYPE_URI, URI_PREFIX_HTTPS, '/', '/', 'e', 's', 'p', 'r', 'e', 's', 's', 'i', 'f', '.', 'c', 'o', 'm',
-};
-
 static uint8_t AppendData(esp_ble_adv_data_type ad_id, uint8_t *data, uint8_t length, uint8_t *buffer)
 {
     uint8_t i = 0;
@@ -148,6 +143,8 @@ static uint32_t counter;
 void app_main(void)
 {
     esp_err_t ret;
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bluedroid_config_t cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
 
     //initialize NVS
     ret = nvs_flash_init();
@@ -159,7 +156,6 @@ void app_main(void)
 
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
 
-    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ret = esp_bt_controller_init(&bt_cfg);
     if (ret) {
         ESP_LOGE(DEMO_TAG, "%s initialize controller failed: %s", __func__, esp_err_to_name(ret));
@@ -172,7 +168,6 @@ void app_main(void)
         return;
     }
 
-    esp_bluedroid_config_t cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
     ret = esp_bluedroid_init_with_cfg(&cfg);
     if (ret) {
         ESP_LOGE(DEMO_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
@@ -197,31 +192,14 @@ void app_main(void)
         return;
     }
 
-    //config adv data
     adv_config_done |= ADV_CONFIG_FLAG;
-    adv_config_done |= SCAN_RSP_CONFIG_FLAG;
+    
     ret = esp_ble_gap_config_adv_data_raw(adv_raw_data, sizeof(adv_raw_data));
     if (ret) {
         ESP_LOGE(DEMO_TAG, "config adv data failed, error code = %x", ret);
         return;
     }
 
-    ret = esp_ble_gap_get_local_used_addr(local_addr, &local_addr_type);
-    if (ret) {
-        ESP_LOGE(DEMO_TAG, "get local used address failed, error code = %x", ret);
-        return;
-    }
-
-    scan_rsp_raw_data[2] = local_addr[5];
-    scan_rsp_raw_data[3] = local_addr[4];
-    scan_rsp_raw_data[4] = local_addr[3];
-    scan_rsp_raw_data[5] = local_addr[2];
-    scan_rsp_raw_data[6] = local_addr[1];
-    scan_rsp_raw_data[7] = local_addr[0];
-    ret = esp_ble_gap_config_scan_rsp_data_raw(scan_rsp_raw_data, sizeof(scan_rsp_raw_data));
-    if (ret) {
-        ESP_LOGE(DEMO_TAG, "config scan rsp data failed, error code = %x", ret);
-    }
     uint16_t tt = 2506;
     uint8_t asd[] = { 0xD2, 0xFC, 0x40, 0x3E, 0x01, 0x02, 0x03, 0x04};
     uint8_t length = BTH_Temp(asd, tt);
@@ -302,6 +280,9 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
             break;
         }
         ESP_LOGI(DEMO_TAG, "Advertising start successfully");
+        break;
+    case ESP_GAP_BLE_GET_DEV_NAME_COMPLETE_EVT:
+        ESP_LOGI(DEMO_TAG, "ESP_GAP_BLE_GET_DEV_NAME_COMPLETE_EVT has come");
         break;
     default:
         break;
